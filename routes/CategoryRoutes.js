2a8262823020e65a49e97f82c081e4a8c4eb75f4
Usman Ali <Usman.tpr@gmail.com>
2024-11-09 21:32:02 +0500

    added

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -4,7 +4,6 @@ const upload = require("../config/multer");
 const { add , getAll } = require("../controllers/CategoryController");
 
 routes.post("/add", upload.single("image") , add)
-routes.post("/add", upload.single("image") , add)
 routes.get("/getAll" , getAll)
 
 module.exports = routes
