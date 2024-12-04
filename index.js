4b846cfc04f8cd84af7211f09eccc20843b18766
Usman Ali <Usman.tpr@gmail.com>
2024-12-04 23:19:26 +0500

    added

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ app.use("/subCategory", SubCategoryRoutes);
 
 // Start the server
 const port = process.env.PORT || 8002;  // Use the port from environment variable or default to 5000
-app.listen(port, '127.0.0.1', () => {
-    console.log("Server running on http://127.0.0.1:8002");
+app.listen(port, () => {
+    console.log("Server running on "+ port);
   });
   
 app.get('/', (req, res) => {
